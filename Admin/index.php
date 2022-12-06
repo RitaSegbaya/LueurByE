7d0ab6e10f153702d3d67947dcf144d1a4097925
Rita Segbaya <rita.segbaya@ashesi.edu.gh>
2022-12-06 17:55:31 +0000

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -66,7 +66,7 @@
                    
                     <a class="btn btn-sm-square btn-outline-body me-0" href="https://www.instagram.com/lueurby.e"><i class="fab fa-instagram"></i></a>
 
-                    <div class="TN bzz aHS-YH" style="margin-left:0px"><div class="qj qr"></div><div class="aio UKr6le"><span class="nU false"><a href="https://meet.google.com/new?hs=180&amp;authuser=0" target="_top" class="J-Ke n0" title="Start a meeting" aria-label="Start a meeting" draggable="false">Start a meeting</a></span></div><div class="nL aif"></div></div>
+                    <a href="https://meet.google.com/new?hs=180&amp;authuser=0" target="_top" class="J-Ke n0" title="Start a meeting" aria-label="Start a meeting" draggable="false">Start a meeting</a>
                 </div>
             </div>
         </div>
