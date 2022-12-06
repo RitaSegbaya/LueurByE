3d8ad4ddd0d0a182f8382a4d9cea8f419d49941c
Rita Segbaya <rita.segbaya@ashesi.edu.gh>
2022-12-06 14:22:21 +0000

    updates

--- Login/register.php
+++ Login/register.php
@@ -1,109 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V16</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-    <script src="../JS/functions.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../logcss/style.css">
+    <title>Register | LueurByE</title>
 </head>
 <body>
-	
-	<!--===============================================================================================-->
-<div class="limiter">
-		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
-			<div class="wrap-login100 p-t-30 p-b-50">
-				<span class="login100-form-title p-b-41">
-					AshGrill
-				</span>
-				<form action="register_process.php" method="POST" class="login100-form validate-form p-b-33 p-t-5">
+    <div class="form">
+        <h1>Sign Up</h1>
+        <form action="registerprocess.php" method="POST">
+            <div class="form-control">
+                <input type="text" placeholder="Full name" name="name" id="name" required>
+            </div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Enter Email">
-						<input class="input100" type="text" name="email" placeholder="email">
-						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
-					</div>
+            <div class="form-control">
+                <input type="email" placeholder="email" name="email" id="email" required>
+            </div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Enter your Full name">
-						<input class="input100" type="text" name="name" placeholder="name" re>
-						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
-					</div>
+            <div class="form-control">
+            
+                <input type="password" class="form-control" placeholder="Enter your password" id="password" name="password"
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" 
+                title="Must contain at least one number and one uppercase 
+                and lowercase letter, and at least 6 or more characters" required>
+            </div>
 
-					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="password" placeholder="Password">
-						<span class="focus-input100" data-placeholder="&#xe80f;"></span>
-					</div>
-					<div class="wrap-input100 validate-input" data-validate = "Enter Phone Number">
-						<input class="input100" type="text" name="phone number" placeholder="phone number">
-						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
-					</div>
+            <!-- <div class="form-control">
+                <input type="text" placeholder="country" name="country" id="country" required>
+            </div>
 
+            <div class="form-control">
+                <input type="text" placeholder="city" name="city" id="city" required>
+            </div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Enter Student id">
-						<input class="input100" type="text" name="student id " placeholder="student id ">
-						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
-					</div>
+            <div class="form-control">
+                <input type="tel" placeholder="contact" name="contact" id="contact" required> -->
+            <!-- </div> -->
 
-					<div class="wrap-input100 validate-input" data-validate = "Enter Business Name">
-						<input class="input100" type="text" name="Business Name" placeholder="Business Name">
-						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
-					</div>
+            <!-- <div class="form-control">
+                <input type="text" name="img"> 
+                <input type="file" name="img" id="img" accept="image/*"> 
+            </div> -->
 
-					<div class="container-login100-form-btn m-t-32">
-						<button class="login100-form-btn" id="register_auser" name="register_auser" onclick="checkExpression(document.form1.number)">
-							Register
-						</button>
-					</div>
+            <div class="form-control">
+                <input type="submit" name="submit" value="REGISTER"> 
+            </div>
 
-					<!-- <div class="container-login100-form-btn m-t-32">
-						<button class="login100-form-btn" id="register_auser" name="register_auser" onclick="ValidateEmail(document.form1.text1)">
-							Register
-						</button>
-					</div> -->
+            <p>Already registered? <a href="login.php">Login</a></p>
+            
+           
+        </form>
+    </div>
 
-				</form>
-			</div>
-		</div>
-	</div>
-	
-
-	<div id="dropDownSelect1"></div>
-	
-<!--===============================================================================================-->
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/daterangepicker/moment.min.js"></script>
-	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-	<script src="js/main.js"></script>
-
+    <script src="../View/js/validation.js"></script>
 </body>
 </html>
