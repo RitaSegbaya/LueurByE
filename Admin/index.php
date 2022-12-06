e5ed8c455b64c5a185d94b63ff5440973fb33d6f
Rita Segbaya <rita.segbaya@ashesi.edu.gh>
2022-12-06 17:47:26 +0000

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -26,15 +26,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="View/lib/animate/animate.min.css" rel="stylesheet">
-    <link href="View/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="View/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <link href="../View/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="../View/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="../View/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="View/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../View/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="View/css/style.css" rel="stylesheet">
+    <link href="../View/css/style.css" rel="stylesheet">
 </head>
 
 <body>
