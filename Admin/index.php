8f9fa9b7bf4150e5eef439d7c6a16f6884e506c7
Rita Segbaya <rita.segbaya@ashesi.edu.gh>
2022-12-06 17:40:01 +0000

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -340,21 +340,21 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/counterup/counterup.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="lib/tempusdominus/js/moment.min.js"></script>
-    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
-    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="View/lib/wow/wow.min.js"></script>
+    <script src="View/lib/easing/easing.min.js"></script>
+    <script src="View/lib/waypoints/waypoints.min.js"></script>
+    <script src="View/lib/counterup/counterup.min.js"></script>
+    <script src="View/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="View/lib/tempusdominus/js/moment.min.js"></script>
+    <script src="View/lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="View/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+    <script src="View/js/main.js"></script>
 </body>
 
 </html>
