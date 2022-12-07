c82238bc1fc6b61b9d2b55e16d5e75428cf06b75
Rita Segbaya <rita.segbaya@ashesi.edu.gh>
2022-12-07 21:37:16 +0000

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     $email=$_SESSION['email'];
-    echo $email;
+    
    
 
 
@@ -49,7 +49,7 @@
         //Email Settings
         $mail->isHTML(true);
         $mail->setFrom('wecreategh2812@gmail.com', 'LueurByE');
-        $mail->addAddress($email); // enter email address whom you want to send
+        $mail->addAddress($to); // enter email address whom you want to send
         $mail->Subject = ($subject);
         $mail->Body = $body;
         if ($mail->send()) {
