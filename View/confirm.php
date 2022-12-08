9487261f5ba9d15cee6f89d43f51cdc8b521202b
RitaSegbaya <94528508+RitaSegbaya@users.noreply.github.com>
2022-12-08 09:55:51 +0000

    Update confirm.php

--- View/confirm.php
+++ View/confirm.php
@@ -83,8 +83,8 @@
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Pages</a>
                     <a href="https://eworldgh.company.site" class="dropdown-item">Main Site</a>
-                       
                 </div>
+                <a href="../Actions/logout.php" class="btn btn-primary py-2 px-4 d-none d-lg-block">Logout</a>
             </div>
             
         </div>
@@ -95,14 +95,7 @@
     <!-- Page Header Start -->
     <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
-            <h1 class="display-1 text-white animated slideInDown">Contact Us</h1>
-            <nav aria-label="breadcrumb animated slideInDown">
-                <ol class="breadcrumb text-uppercase mb-0">
-                    <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
-                    <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
-                    <li class="breadcrumb-item text-primary active" aria-current="page">Contact Us</li>
-                </ol>
-            </nav>
+            <h1 class="display-1 text-white animated slideInDown">Congratulations🥳</h1>
         </div>
     </div>
     <!-- Page Header End -->
@@ -113,7 +106,7 @@
         <div class="container">
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
                 <h4 class="section-title">Congratulations!</h4>
-                <h1 class="display-5 mb-4">Your Booking is Complete</h1>
+                <h1 class="display-5 mb-4">Your Booking is Complete!</h1>
             </div>
             
         </div>
